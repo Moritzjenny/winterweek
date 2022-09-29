896c0a02e8b12a4aa4226c0b822bbb2d09f5e303
Moritzjenny <moritzjenny@hotmail.com>
2022-09-29 21:03:21 +0200

    Add header image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import forest from './images/forest.jpg';
 import './App.css';
 
 function App() {
@@ -12,12 +13,14 @@ function App() {
 
   return (
       <div className="App">
-        <header className="App-header">
+          <div className="header-image" style={{ backgroundImage: `url(${forest})`}}>
+              <div className="container" style={{minHeight: '350px'}}>
+                  <div className="container" style={{minHeight: '50px'}}></div>
+                  <p style={{ margin: 0, fontSize: 'max(3vw, 60px)'}}> Winter Week </p>
+                  <p style={{fontSize: 'max(1vw, 20px)'}}> 27.01.2023 - 03.02.2023</p>
+              </div>
+          </div>
 
-          Winter Week 2023
-
-          <p>{message}</p>
-        </header>
       </div>
   );
 }
